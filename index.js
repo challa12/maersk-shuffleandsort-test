dc3ab6f4bdbccefc748f8e3bb89dd19b940452e5
Siva Reddy CH <sivareddy709@gmail.com>
2020-07-24 17:05:33 +0530

    Added custom fonts

--- index.js
+++ index.js
@@ -2,44 +2,48 @@
 
 
 /** Event listener on window resize to indentify desktop & mobile resolution */
-window.addEventListener('resize', applyColors);
+window.addEventListener('resize', () => {
+    applyColors();
+});
 
-/** sApply colors on initial launch */
+/** Sort the Gird Apply colors on initial launch */
 setTimeout(() => {
     sort();
 }, 0)
 
-/** function to apply the background color / border to each div item in the Grid List */
-function applyColors() {
-    /** Given Set of Colors taken as array */
-    var colorArray = ['#6F98A8', '#2B8EAD', '#2F454E', '#2B8EAD', '#2F454E', '#BFBFBF', '#BFBFBF', '#6F98A8', '#2F454E'];
 
-    if (window.innerWidth >= 768) { // Desktop
+
+/** function to apply the background color / border to each div item in the Grid List */
+const applyColors = () => {
+    /** Given Set of Colors taken as array */
+    let colorArray = ['#6F98A8', '#2B8EAD', '#2F454E', '#2B8EAD', '#2F454E', '#BFBFBF', '#BFBFBF', '#6F98A8', '#2F454E'];
+
+    if (window.innerWidth <= 767) { // non-dekstop
+        var listItems = document.getElementsByClassName('list-item');
+        for (var i = 0; i < listItems.length; i++) {
+            listItems[i].style.backgroundColor = '#EFEFEF';
+            listItems[i].style.borderLeft = `10px solid ${colorArray[i]}`; // Template String
+        }
+    } else { // Desktop
         var listItems = document.getElementsByClassName('list-item');
         for (var i = 0; i < listItems.length; i++) {
             listItems[i].style.backgroundColor = colorArray[i];
         }
-    } else { // non-dekstop
-        var listItems = document.getElementsByClassName('list-item');
-        for (var i = 0; i < listItems.length; i++) {
-            listItems[i].style.backgroundColor = '#EFEFEF';
-            listItems[i].style.borderLeft = `10px solid ${colorArray[i]}`;
-        }
     }
 }
 
+
 /** function to shuffle the list cards */
-function shuffle() {
-    var section = document.querySelector('section');
-    for (var i = section.children.length; i >= 0; i--) {
+const shuffle = () => {
+    let section = document.querySelector('section');
+    for (let i = section.children.length; i >= 0; i--) {
         section.appendChild(section.children[Math.random() * i | 0]);
     }
 }
 
-/** function to sort the back to 1-9 */
-function sort() {
+const sort = () => {
     const maxNumber = 9;
-    var listContainer = document.querySelector('section');
+    let listContainer = document.querySelector('section');
     listContainer.innerHTML = '';
     for (var i = 1; i <= maxNumber; i++) {
         listContainer.appendChild(createDivItem(i));
@@ -47,7 +51,7 @@ function sort() {
     applyColors();
 }
 
-function createDivItem(val) {
+const createDivItem = val => {
     let div = document.createElement('div');
     div.className = 'list-item'
     div.textContent = val;
